d7a7fb78e9d111cd6356d70795a788eab322529d
Afelie <afelie@buffalo.edu>
2022-09-28 17:53:24 -0400

    Test final version all lines

--- week_04/case_study_04Afelie.R
+++ week_04/case_study_04Afelie.R
@@ -4,3 +4,14 @@ library(tidyverse)
 library(nycflights13)
 
 str(flights)
+str(airports)
+
+farthest_airport <- flights %>%
+  arrange(desc(distance)) %>%
+  slice(1:5)%>% 
+  left_join(airports, by = c("origin" = "faa"))%>%
+  select(dest)%>%
+  as.character()
+
+farthest_airport
+
