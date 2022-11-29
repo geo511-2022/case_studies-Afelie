c31f4ea179dbe1b68b85466e27ad3e84d96e921d
Afelie <afelie@buffalo.edu>
2022-11-29 14:20:43 -0500

    Finished

---
+++ week_12/case_study_12AFELIE.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+library(htmlwidgets)
+library(widgetframe)
+library(tidyverse)
+library(rnoaa)
+library(xts)
+library(dygraphs)
+
+d=meteo_tidy_ghcnd("USW00014733",
+                   date_min = "2016-01-01", 
+                   var = c("TMAX"),
+                   keep_flags=T) %>% 
+  mutate(date=as.Date(date),
+         tmax=as.numeric(tmax)/10) #Divide the tmax data by 10 to convert to degrees.
+
+x <- xts(x = d$tmax, order.by=d$date)
+
+xdy <- dygraph(x, main = "Daily Maximum Temperature in Buffalo, NY",) %>%
+  dyRangeSelector(dateWindow = c("2020-01-01", "2020-10-31"))
+
+xdy
